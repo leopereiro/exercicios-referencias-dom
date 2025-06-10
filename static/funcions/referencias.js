717d1a160115619d342cc813e2eee1cd3785b1b9
leopereiro <lperpom.genius@gmail.com>
2025-06-10 14:21:02 +0200

    exercicio 4

--- static/funcions/referencias.js
+++ static/funcions/referencias.js
@@ -1,4 +1,4 @@
-export { refP, refDiv, refLista, refTable, refTh, refTd, refBody, refBoton, refPEx3, refBotonP, refListaEx3, refBotonLista, Lista2Ex3, refSpanLista, DivEx4Before, DivEx4After, elementoDivEx4}
+export { refP, refDiv, refLista, refTable, refTh, refTd, refBody, refBoton, refPEx3, refBotonP, refListaEx3, refBotonLista, Lista2Ex3, refSpanLista, DivEx4Before, DivEx4After, elementoDivEx4, elementoPEx4}
 
 const refP = document.querySelector("#ref-p");
 const refDiv = document.querySelector("#ref-div");
@@ -20,10 +20,70 @@ let refImaxe1 = document.querySelector("#imaxe-1");
 let DivEx4Before = document.createElement("div");
 let DivEx4After = document.createElement("div");
 let elementoDivEx4 = document.querySelector("#div-ex-4");
+let elementoPEx4 = document.createElement("p");
+let botonEx4 = document.querySelector("#boton-ex-4");
+let imaxeElementoP = document.createElement("img");
+imaxeElementoP.src = "./imaxes/rosa.jpg"
+let botonListaEx4 = document.querySelector("#boton-lista-ex-4")
+let listaEx4 = document.createElement("ul");
+let texto1ListaEx4 = document.createElement("li");
+let texto2ListaEx4 = document.createElement("li");
+let texto3ListaEx4 = document.createElement("li");
+let texto4ListaEx4 = document.createElement("li");
+let texto5ListaEx4 = document.createElement("li");
+let imaxeListaEx4 = document.createElement("img");
+let elementoInputEx4 = document.querySelector("input");
+let idTextoPintado = document.querySelector("#textoPintado");
+let botonInputEx4 = document.querySelector("#boton-input-ex-4");
+
+botonInputEx4.addEventListener("click", (e) => {
+    console.log("O texto é:", input.value);
+
+let etiquetaP = document.createElement("p");
+etiquetaP.innerHTML = "O texto é:" + input.value
+elementoInputEx4.after(etiquetaP);
+
+});
+
+
+
+
+
 DivEx4Before.innerHTML = "Novo texto before";
 DivEx4After.innerHTML = "Novo texto after";
+elementoPEx4.innerHTML = "Novo elemento P"
 elementoDivEx4.before(DivEx4Before);
 elementoDivEx4.after(DivEx4After);
+elementoPEx4.append(imaxeElementoP);
+
+listaEx4.innerHTML = "Lista Exercicio 6";
+texto1ListaEx4.innerHTML = "Texto 1"
+texto2ListaEx4.innerHTML = "Texto 2"
+texto3ListaEx4.innerHTML = "Texto 3"
+texto4ListaEx4.innerHTML = "Texto 4"
+texto5ListaEx4.innerHTML = "Texto 5"
+
+imaxeListaEx4.src = "./imaxes/estrella.png"
+
+
+botonEx4.addEventListener("click", ()=> {
+    document.body.append(elementoPEx4)
+})
+
+botonListaEx4.addEventListener("click", ()=>{
+    document.body.append(listaEx4)
+})
+
+listaEx4.append(texto1ListaEx4)
+listaEx4.append(texto2ListaEx4)
+listaEx4.append(texto3ListaEx4)
+listaEx4.append(texto4ListaEx4)
+listaEx4.append(texto5ListaEx4)
+texto1ListaEx4.append(imaxeListaEx4)
+
+
+
+
 
 /*refP.addEventListener("click", ()=> {
     refP.firstChild.textContent = "cambio o texto"
--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-import {refP, refDiv, refLista, refTable, refTh, refTd, refBody, refBoton, refPEx3, refBotonP, refListaEx3, refBotonLista, Lista2Ex3, refSpanLista, DivEx4Before, DivEx4After, elementoDivEx4} from "./funcions/referencias.js"
+import {refP, refDiv, refLista, refTable, refTh, refTd, refBody, refBoton, refPEx3, refBotonP, refListaEx3, refBotonLista, Lista2Ex3, refSpanLista, DivEx4Before, DivEx4After, elementoDivEx4, elementoPEx4} from "./funcions/referencias.js"
 
 /*console.log(refP)
 console.log(refDiv)
